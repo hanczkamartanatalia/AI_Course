0274141f90f8e54b33a9f4e951afe7d2d7b1b4c0
Natalia Hańczka <UL0269767@edu.uni.lodz.pl>
2023-05-16 16:53:32 +0200

    Add corrected working adding user and login

--- lib/register-bg.php
+++ lib/register-bg.php
@@ -37,71 +37,41 @@ $connect = connectToDataBase();
 
 //-------------------------SQL---------------------------------------------------//
 
-// $login = mysqli_real_escape_string($connect, $login);
-
-// $sql_unique_login = "SELECT * FROM Login WHERE login = $login";
-
-// $result = $connect->query($sql);
-
-// // if (!$result = $connect->query($sql)) // error with connect
-// // {
-// //     $connect->close();
-// //     header('Location: ../user/login-bg_copy.php');
-// //     exit();
-// // }
-
-// if($result->num_rows != 0)
-// {
-//     $connect->close();
-//     header('Location: ../user/sql_bg.php');
-//     exit();
-// }
-
-
-$host = "localhost";
-$user = "2024_nhanczka";
-$pass = "Niebo.164";
-$dbname = "2024_nhanczka";
-$prefix = "prefix nazwy tabeli";
-
-// Create connection
-$link = new mysqli($host, $user, $pass, $dbname);
-
 $sqlLogin = "INSERT INTO `Login`(`login`, `login_Password`) VALUES ('$login', '$password')";
 
-if ($link -> query($sqlLogin) === TRUE) 
+if ($connect -> query($sqlLogin) === TRUE) 
 {
     echo "New record created successfully";
 }
  else 
 {
-    echo "Error: " . $sql;
+    header('Location: register.php'); 
 }
 
- //$result->free();
- $link->close();
+$sql = "SELECT * FROM `Login` WHERE login = '$login'";
+
+$result = $connect -> query($sql);
+
+if($result)
+{
+    $listDataOfAccount = $result->fetch_assoc();
+    $id_fk = $listDataOfAccount['id_login'];
+}
+else
+{
+    echo 'Error';
+}
+
+$sql = "INSERT INTO `Application_user`(`Name`, `Last_name`, `Email`,`id_login_FK`) VALUES ('$name','$lastname','$email','$id_fk')";
+
+$result = $connect -> query($sql);
+
+if($result)
+{
+    echo 'Created accaunt correct.';
+}
 
 
-// $sql= "SELECT id_login FROM `Login` WHERE login = $login";
-
-// $result = $connect->query($sql);
-
-// $listDataOfAccount = $result->fetch_assoc();
-
-// $id_fk = $listDataOfAccount['id_login'];
-
-// $sqlUser = "INSERT INTO `Application_user`(`Name`, `Last_name`, `Email`,`id_login_FK`) VALUES ($name,$lastname,$email,$id_fk)";
-
-// $_SESSION['login'] = $login;
-// $_SESSION['name'] = $name;
-// $_SESSION['last_name'] = $lastname;
-// $_SESSION['email'] = $email;
-// $_SESSION['id_login_fk'] = $id_fk;
-
-// $id_fk->free();
-// $result->free();
-// $connect->close();
-
-// header('Location: ../user/account.php'); // jeśli strona nie zostanie znaleziona to wykonuje polecenie dalej
-
-// ?>
+ $result->free();
+ $connect->close();
+  ?>
